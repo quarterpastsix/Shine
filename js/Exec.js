ad9e3a558b72cad298896c96d4a940606b363516
Gotti <sjshshwa@gmail.com>
2019-09-22 22:50:08 +0200

    added basic connection

--- js/Exec.js
+++ js/Exec.js
@@ -2,8 +2,5 @@
 import { Game } from "./Logic.js";
 
 
-console.log("Henlo World");
-document.write("hello world");
-
 let game = new Game();
-game.greet();
+game.start();
--- js/Logic.js
+++ js/Logic.js
@@ -1,8 +1,49 @@
-
+"use strict";
 
 export class Game{
 
-    greet(){
+    constructor(){
+
+        this.gameDiv = document.getElementById("Game");
+
+        this.socket = null;
+
+        let startbutton = document.createElement("Button");
+        startbutton.innerText = "Connect and Start";
+        startbutton.addEventListener("click",()=>{
+            this.socket = new WebSocket("ws://qpserving.ddns.net:8765");
+            this.socket.onopen = ()=>{
+                this.gameDiv.remove(startbutton);
+                this.socket.send("hi")
+            };
+            this.socket.onerror = (error) =>{
+                console.log("not connected");
+                console.log(error);
+                startbutton.disabled = true;
+            };
+            this.socket.onclose = (event)=>{
+                console.log("close");
+                console.log(event)
+            };
+            this.socket.onmessage = (event) =>{
+                // console.log(event);
+                console.log(event.data);
+            };
+        });
+
+
+        let canv = document.createElement("canvas");
+        canv.width = window.innerWidth;
+        canv.height = window.innerheight;
+
+        this.gameDiv.appendChild(startbutton);
+        this.gameDiv.appendChild(canv);
+
+        this.ctx = canv.getContext("2d");
+
+    }
+
+    start(){
         console.log("hi")
     }
 
